0404f40e711eac1ce80caebea2025b0ffac1e88e
Jiyeong <eizishwiwab@gmail.com>
2025-10-16 20:37:21 +0900

    Gradle 설정 복구 및 최종 정리

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -28,5 +28,5 @@ plugins {
     id("com.android.application") apply false
     id("com.android.library") apply false
     id("org.jetbrains.kotlin.android") apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
